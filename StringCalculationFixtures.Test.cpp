90cd78fbc546961b8d7d88db73e431902aaa20c2
Akash Sharma <Akash.Sharma@in.bosch.com>
2024-10-28 17:07:27 +0530

    Update StringCalculationFixtures.Test.cpp

--- StringCalculationFixtures.Test.cpp
+++ StringCalculationFixtures.Test.cpp
@@ -26,3 +26,39 @@ TEST_F(StringCalculatorAddFixture, Returns_0_For_0){
     int actualValue=objUnderTest->Add(input);
     ASSERT_EQ(actualValue,expectedValue);
 }
+TEST_F(StringCalculatorAddFixture, returns_1_for_one){
+    string input="1";
+    int expectedValue=1;
+    int actualValue=objUnderTest->Add(input);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST_F(StringCalculatorAddFixture, two_comma_numbers_returns_their_sum){
+    string input="1,2";
+    int expectedValue=3;
+    int actualValue=objUnderTest->Add(input);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST_F(StringCalculatorAddFixture,  multiple_comma_numbers_returns_their_sum){
+    string input="2,3,4";
+    int expectedValue=9;
+    int actualValue=objUnderTest->Add(input);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST_F(StringCalculatorAddFixture,  newline_and_comma_returns_their_sum){
+    string input="1\n2,3";
+    int expectedValue=6;
+    int actualValue=objUnderTest->Add(input);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST_F(StringCalculatorAddFixture,  delimiter_returns_the_sum_based_on_that_delimiter){
+    string input="//;\n1;2";
+    int expectedValue=3;
+    int actualValue=objUnderTest->Add(input);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST_F(StringCalculatorAddFixture,  numbers_over_1000_ignore){
+    string input="37,1456,6";
+    int expectedValue=43;
+    int actualValue=objUnderTest->Add(input);
+    ASSERT_EQ(actualValue,expectedValue);
+}
